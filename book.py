d91812cc3d380cd26cfc91f4a94f65f6b5d778db
username123321246 <george.mircea.vrana@gmail.com>
2025-06-17 17:55:06 +0200

    Update book.py

--- book.py
+++ book.py
@@ -4,6 +4,59 @@ from tkinter import messagebox
 from tkinter.filedialog import *
 
 mainwin = Tk()
+addressbook = {}
+
+def clear_all():
+    name_entry.delete(0,END)
+    address_entry.delete(0, END)
+    mobile_entry.delete(0, END)
+    email_entry.delete(0, END)
+    
+
+def update():
+    key = name_entry.get()
+    if key == "":
+        messagebox.showinfo("Error","Name can't be empty")
+    else:
+        if key not in addressbook.keys():
+            listbox.insert(END, key)
+        addressbook[key] = (address_entry.get(), mobile_entry.get(), email_entry())
+        clear_all()
+            
+
+def saveFile():
+        fout = asksaveasfile(defaultextension = ".txt")
+        if fout is not None:
+            for item in list.get(0, END):
+                print(item.strip(), file = fout)
+                list.delete(0,END)
+
+def edit():
+     clear_all()
+     index = listbox.curselection()
+     if index:
+        name_entry.insert(0, listbox.get(index))
+        details = addressbook[name_entry.get()]         
+     #get details corresponding to anem from dictioary
+        address_entry.insert(0, details[0])
+        mobile_entry.insert(0, details[1])
+        email_entry.insert(0, details, [2])
+     else:
+        messagebox.showinfo("Error", "Information is missing.")
+def delete():
+    index = listbox.curselection()
+    if index:
+        del addressbook[listbox.get(index)]
+        listbox.delete(index)
+        clear_all()
+    else:
+        messagebox.showinfo("Error", "Select a name.")
+
+
+
+
+
+
 
 mainwin.title("My Adress Book")
 maintitle = Label(mainwin, text = "My Adress Book")
@@ -39,22 +92,17 @@ email_label.grid(row = 4, column = 3)
 email_entry = Entry(mainwin)
 email_entry.grid(row = 4, column = 4)
 
-edit_button = Button(mainwin, text = "Edit", command = None)
+edit_button = Button(mainwin, text = "Edit", command = edit)
 edit_button.grid(row = 6, column = 0)
 
-delete_button = Button(mainwin, text = "Delete", command = None)
+delete_button = Button(mainwin, text = "Delete", command = delete)
 delete_button.grid(row = 6, column = 1)
 
-update_button = Button(mainwin, text = "Update/Add", command = None)
+update_button = Button(mainwin, text = "Update/Add", command = update)
 update_button.grid(row = 6, column = 4)
 
-save_button = Button(mainwin, text = "Save", width = 40, command = None)
+save_button = Button(mainwin, text = "Save", width = 30, command = saveFile)
 save_button.grid(row = 7, column = 1, columnspan = 3)
-
-
-
-
-
 
 
 
